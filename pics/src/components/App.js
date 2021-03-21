fd2e74c3bc64790415124aa224b68be6bb19d0ec
zdrale <nikolazd22@outlook.com>
2021-03-21 23:26:26 +0100

    Made request to the API
    --Enabled pictures fetching with axios.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,9 +3,16 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+   
+    state = {images: []};
 
-    onSearchSubmit(term) {
-        console.log(term);
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos',{
+            params: {query: term },
+            headers: { Authorization: 'Client-ID gZo4lLGs2VcRZKzmw3Me0WgHsvUfPN7vmlbNAeXGnhw'}
+        });
+
+        this.setState({images:response.data.results});
     }
 
     
@@ -14,7 +21,7 @@ class App extends React.Component {
         return ( <div className="ui container" style={{marginTop: '10px'}}>
 
             <SearchBar onSubmit={this.onSearchSubmit} />
-            
+            Found: {this.state.images.length} images    
             </div>      
         );
     }
