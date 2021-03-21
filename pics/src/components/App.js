4bee21647750b4fcf963de8a2a0759c9260b6eb3
zdrale <nikolazd22@outlook.com>
2021-03-21 22:09:42 +0100

    Conversion to class based component
    --added OnFormSubmit function

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return <div className="ui container" style={{marginTop: '10px'}}>
+class App extends React.Component {
 
-     <SearchBar />
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
     
-    </div>
+    render() {
 
-};
+        return ( <div className="ui container" style={{marginTop: '10px'}}>
+
+            <SearchBar onSubmit={this.onSearchSubmit} />
+            
+            </div>      
+        );
+    }
+}
 
 export default App;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -6,11 +6,17 @@ class SearchBar extends React.Component {
 
     state = {term: ''}
 
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
+    }
+    
 
     render() {
         return ( 
             <div className="ui-segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text"
