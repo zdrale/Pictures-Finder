40a128da5387eb55b12b9f1808c1346b83f1fda6
zdrale <nikolazd22@outlook.com>
2021-03-22 10:02:24 +0100

    Rendered List of images after form submit

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,15 +1,16 @@
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import React from 'react';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
    
     state = {images: []};
 
     onSearchSubmit = async (term) => {
-        const response = await axios.get('https://api.unsplash.com/search/photos',{
+        const response = await unsplash.get('/search/photos',{
             params: {query: term },
-            headers: { Authorization: 'Client-ID gZo4lLGs2VcRZKzmw3Me0WgHsvUfPN7vmlbNAeXGnhw'}
+           
         });
 
         this.setState({images:response.data.results});
@@ -18,10 +19,11 @@ class App extends React.Component {
     
     render() {
 
-        return ( <div className="ui container" style={{marginTop: '10px'}}>
+        return ( 
+            <div className="ui container" style={{marginTop: '10px'}}>
 
-            <SearchBar onSubmit={this.onSearchSubmit} />
-            Found: {this.state.images.length} images    
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                <ImageList images = {this.state.images}/>
             </div>      
         );
     }
